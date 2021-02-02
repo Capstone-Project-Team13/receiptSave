f075aa3141433fd4cc00396c657dc652c6e2f8bf
ailinzhang <az007@uark.edu>
2021-02-02 14:26:44 -0600

    Main menu update with cards and icons.

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -1,31 +1,35 @@
 package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 //ailin commit test...
     //Michael test commit
     private Button btnAction;
-
+    private CardView scanCard;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btnAction = findViewById(R.id.btnAction);
-        btnAction.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                //Move from main page to scan page
-                Intent intent = new Intent(MainActivity.this, Scan.class);
-              //  Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivity(intent);
-            }
-        });
+        scanCard = (CardView)  findViewById(R.id.scan_receipt_card);
+        scanCard.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.scan_receipt_card: i = new Intent(this,Scan.class);startActivity(i);break;
+            default:break ;
+        }
     }
 }
