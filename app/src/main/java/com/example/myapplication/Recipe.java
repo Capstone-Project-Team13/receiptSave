ccc0647ff50c5129ef03afd2bcfbdb74a720be64
Jaei Ryu <jaeiryu@uark.edu>
2021-02-20 23:39:04 -0600

    Get data from BD and enable recipe search

--- app/src/main/java/com/example/myapplication/Recipe.java
+++ app/src/main/java/com/example/myapplication/Recipe.java
@@ -1,14 +1,83 @@
 package com.example.myapplication;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
 
 public class Recipe extends AppCompatActivity {
+
+    private ListView listView;
+    private ArrayAdapter adapter;
+    private DatabaseReference mDatabase;
+    private ArrayList<String> itemList = new ArrayList<String>();
+    private ArrayList<String> searchList = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_recipe);
+
+        listView = findViewById(R.id.listId);
+        mDatabase = FirebaseDatabase.getInstance().getReference("ReceiptItems");
+
+        getData();
+    }
+
+    private void getData(){
+        mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                for(DataSnapshot snapshot:dataSnapshot.getChildren()){
+                    String item = snapshot.child("item").getValue().toString();
+                    itemList.add(item);
+                    //Log.d("Recipe", "what?????: " + snapshot.getValue());
+                }
+                adapter = new ArrayAdapter<String>(getApplicationContext(), android.R.layout.simple_expandable_list_item_1, itemList);
+                listView.setAdapter(adapter);
+
+                listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                    @Override
+                    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        if (!searchList.contains(itemList.get(position))) {
+                            searchList.add(itemList.get(position));
+                        }
+                        //Log.d("Recipe", "what?????: " + searchList.size());
+                    }
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+                // Getting Post failed, log a message
+                Log.w("FireBaseData", "loadPost:onCancelled", databaseError.toException());
+            }
+        });
+    }
+
+    public void searchRecipe(View view) {
+        String searchString = " ";
+        for (int i=0; i<searchList.size(); i++){
+            searchString = searchString + searchList.get(i);
+        }
+        searchString = searchString + "food recipe";
+        Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com/search?q="+searchString));
+        startActivity(browserIntent);
     }
 }
